9ecbc134b37284ca38263ca05a92087353b001ac
曹一梁 <caoyiliang@me.com>
2025-02-18 16:22:54 +0800

    更新版本号至 9.2.1 和 9.4.1
    将项目文件 `HJ212.csproj` 中的版本号从 `9.2.0` 更新为 `9.2.1`。
    同时，将 `CSoft.Protocol.ProtocolInterface` 的版本号从 `9.4.0` 更新为 `9.4.1`。

--- HJ212/GB.cs
+++ HJ212/GB.cs
@@ -104,7 +104,7 @@ namespace HJ212
         /// <inheritdoc/>
         public event RespondedLogEventHandler? OnReceivedData { add => _pigeonPort.OnReceivedData += value; remove => _pigeonPort.OnReceivedData -= value; }
         /// <inheritdoc/>
-        public GB(string name, IPhysicalPort physicalPort, string mn, string pw = "123456", bool qn = true, ST st = ST.大气环境污染源, Version version = Version.HJT212_2017)
+        public GB(string name, IPhysicalPort physicalPort, string mn, string pw = "123456", bool qn = true, ST st = ST.大气环境污染源, Version version = Version.HJT212_2017, bool isDisconnectedWhenZero = true)
         {
             _name = name;
             MN = mn;
@@ -123,7 +123,7 @@ namespace HJ212
                 {
                     return Task.FromResult(new GetDataLengthRsp() { Length = 4, StateCode = Parser.StateCode.Success });
                 }
-            })))
+            }), isDisconnectedWhenZero))
             {
                 CheckEvent = async (byte[] bytes) =>
                 {
