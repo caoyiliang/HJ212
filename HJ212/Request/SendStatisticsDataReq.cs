b1b67f7a3b116fbcd9c932beb6197a71dcfd46e2
曹一梁 <caoyiliang@me.com>
2025-02-20 15:22:56 +0800

    更新版本号并优化字符串构建逻辑
    在 `HJ212.csproj` 文件中，将版本号从 `9.2.5` 更新为 `9.2.6`。
    
    在 `SendStatisticsDataReq.cs` 文件中的 `ToBytes` 方法中，修改了字符串构建逻辑，增加了对 `Min` 和 `Max` 属性的空值检查，以确保在这些属性为 `null` 时不会引发异常，同时保持了原有的功能。

--- HJ212/Request/SendStatisticsDataReq.cs
+++ HJ212/Request/SendStatisticsDataReq.cs
@@ -8,7 +8,7 @@ namespace HJ212.Request
     {
         public byte[] ToBytes()
         {
-            var rs = $"{(qn ? $"QN={DateTime.Now:yyyyMMddHHmmssfff};" : "")}ST={(int)st};CN={(int)cn};PW={pw};MN={mn};Flag={(pnum > 1 ? $"{2 | (int)GB._version};PNUM={pnum};PNO={pno}" : $"{0 | (int)GB._version}")};CP=&&DataTime={dataTime:yyyyMMddHHmmss};{string.Join(";", data.Select(c => $"{(c.Cou != null ? $"{c.Name}-Cou={c.Cou}," : "")}{c.Name}-Min={c.Min},{c.Name}-Avg={c.Avg},{c.Name}-Max={c.Max}{(c.Flag != null ? $",{c.Name}-Flag={c.Flag}" : "")}"))}&&";
+            var rs = $"{(qn ? $"QN={DateTime.Now:yyyyMMddHHmmssfff};" : "")}ST={(int)st};CN={(int)cn};PW={pw};MN={mn};Flag={(pnum > 1 ? $"{2 | (int)GB._version};PNUM={pnum};PNO={pno}" : $"{0 | (int)GB._version}")};CP=&&DataTime={dataTime:yyyyMMddHHmmss};{string.Join(";", data.Select(c => $"{(c.Cou != null ? $"{c.Name}-Cou={c.Cou}," : "")}{(c.Min != null ? $"{c.Name}-Min={c.Min}," : "")}{c.Name}-Avg={c.Avg}{(c.Max != null ? $",{c.Name}-Max={c.Max}" : "")}{(c.Flag != null ? $",{c.Name}-Flag={c.Flag}" : "")}"))}&&";
             rs = GB.GetGbCmd(rs);
             return Encoding.ASCII.GetBytes(rs);
         }
